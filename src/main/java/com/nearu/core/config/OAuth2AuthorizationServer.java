78ed3db6fdcd7b2a1164521058d90abfa6e83316
zhaozhenhua <zhaozhenhua1@xiaomi.com>
2019-05-09 18:16:17 +0800

    implicit model

--- src/main/java/com/nearu/core/config/OAuth2AuthorizationServer.java
+++ src/main/java/com/nearu/core/config/OAuth2AuthorizationServer.java
@@ -1,8 +1,6 @@
 package com.nearu.core.config;
 
-import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -20,10 +18,16 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         //super.configure(clients);
         clients.inMemory()
+                //client 和 secret 是客户凭证， 正常应该是从授权服务器注册获取
                 .withClient("clientApp")
                 .secret("123123123")
+
                 .redirectUris("http://zg.test.mi.com")
-                .authorizedGrantTypes("authorization_code")
+                //授权码模式
+                //.authorizedGrantTypes("authorization_code")
+                //简化模式
+                .authorizedGrantTypes("implicit")
+                .accessTokenValiditySeconds(120)
                 .scopes("read_userInfo", "read_contacts");
     }
 }
